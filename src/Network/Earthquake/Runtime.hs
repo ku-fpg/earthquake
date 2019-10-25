d120d262900d55346bdafa2b599353256fdd316b
Andy Gill <andygill@ku.edu>
2019-10-25 12:39:21 -0500

    More general API for runtime
    The initial state is now computed.

--- src/Network/Earthquake/Runtime.hs
+++ src/Network/Earthquake/Runtime.hs
@@ -18,66 +18,66 @@ import Network.Earthquake.Cmd
 import Network.Earthquake.Remote
 import Network.Earthquake.Widget
 
-data RuntimeState model = RuntimeState
-  { theModel :: !model
-  , theTick  :: !Int
-  , theResp  :: !(TChan (Msg model))
-  }
-
 runtime :: forall model m .
 	   (Show model, Widget model, Update model m)
         => (forall a b . Monoid a => m a b -> (a,b))
-	-> model
+	-> m (Cmd (Msg model)) model
 	-> Application -> Application
 runtime run m = start $ jsbRuntime run m
 
 jsbRuntime :: forall model m .
            (Show model, Widget model, Update model m)
         => (forall a b . Monoid a => m a b -> (a,b))
-        -> model
+	-> m (Cmd (Msg model)) model
         -> Engine -> IO ()
 jsbRuntime run m e = do
   print "elmArch"
+  theResp <- newTChanIO
   let render :: Widget model 
-      	     => RuntimeState model
+      	     => model
+	     -> Int
              -> IO ()
-      render state@RuntimeState{..} = do
+      render theModel theTick = do
         let theView = view theModel
         let s0 = 0
         let (json,_) = runState (sendRemote theView) 0
         print ("json",json)
         sendA e $ command $ call "jsb.render" [value (0::Int),value json]
-        wait state theView
+        wait theModel theTick theView
 
       wait :: Widget model
-      	   => RuntimeState model
+      	   => model
+	   -> Int 
            -> Remote (Msg model)
 	   -> IO ()
-      wait state@RuntimeState{..} theView = do 
+      wait theModel theTick theView = do 
         msg <- listen e
         print "waiting for event"
         print msg
         case fromJSON msg :: Result ResponseEvent of
           Error msg -> do
             print("Error fromJSON msg",msg)
-            wait state theView
+	    wait theModel theTick theView
           Success msg' -> do
             print ("got msg",msg')
             case evalState (recvRemote theView msg') 0 of
               Nothing -> do
                 print "no match found for event"
-                wait state theView
-              Just theMsg -> do
-	        let (cmds,theModel') = run $ update theMsg theModel
-		() <- spawnCmd theResp cmds
-                render $ state { theModel = theModel'
-                               , theTick = theTick + 1
-                               }
-  ch <- newTChanIO
-  render $ RuntimeState { theModel = m
-                        , theTick = 0
-			, theResp = ch
-                        }
+                wait theModel theTick theView
+              Just theMsg -> act theTick $ update theMsg theModel
+
+      act :: Widget model 
+      	  => Int
+	  -> m (Cmd (Msg model)) model
+	  -> IO ()
+      act theTick modelAct = do 
+      	let (cmds,theModel) = run modelAct
+	() <- spawnCmd theResp cmds
+        render theModel $ theTick + 1
+
+--  let (cmds,m') = run m
+--  () <- spawnCmd ch cmds
+  act 0 m 
 
 
 ------------------------------------------------------------------------------
