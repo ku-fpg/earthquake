42d8686d9e165fa2bc5e34cbb2849f7a48917403
Andy Gill <andygill@ku.edu>
2019-11-02 18:04:35 -0500

    Fixing bugs

--- examples/ToDo.hs
+++ examples/ToDo.hs
@@ -66,20 +66,25 @@ instance Widget Task where
       , ("focus"       , wait Focus )
       , ("edit"        , Edit <$> recv )
       , ("cancel"      , wait Cancel )
+      , ("commit"      , wait Commit )
       , ("completed"   , Completed <$> recv )
       , ("delete"      , wait Delete )
       ]
   view NoTask = object [("type" , tag "NoTask")]
 
 instance ApplicativeUpdate Task where
+  -- Focus means that you are actively editing
   updateA Focus m@Task{description}  = pure $ m { edits = Just description }
+  -- An edit message updates the internal edit field.
   updateA (Edit description) m       = pure $ m { edits = Just description }
+  -- cancel leaves the edit state (aka unfocus?)
   updateA Cancel m                   = pure $ m { edits = Nothing }
-  updateA Commit m@Task{edits,description} = case edits of
+  -- can commit changes to this entry
+  updateA Commit m@Task{edits} = case edits of
     Nothing -> pure m
     Just raw | T.null raw -> pure NoTask
              | otherwise -> pure $ m { edits = Nothing
-	     	       		     , description = description 
+	     	       		     , description = raw
 				     }
   updateA (Completed bool) m = pure $ m { completed = bool }
   updateA Delete _ = pure NoTask
